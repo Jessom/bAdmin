6ac2b2a021ab0e2e7624cde749a49c8701e8b14a
watasi <jessoncheung@gmail.com>
2018-05-18 13:33:26 +0800

    test:

---
+++ src/views/Index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="container">
+		<HeaderComponent>
+		</HeaderComponent>
+	</div>
+</template>
+
+<script>
+import HeaderComponent from '@/components/HeaderComponent.vue'
+export default {
+	name: 'Index',
+	components: {
+		HeaderComponent
+	}
+}
+</script>
+
+<style>
+</style>
